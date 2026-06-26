0f64dc1789a1cd8d04efdec8ff48a4a7043fa1b6
Shubham Nagar <shubhamnagar234@gmail.com>
2026-06-26 09:40:57 +0530

    Building AI Agents & LCEL Logic

--- tools.py
+++ tools.py
@@ -2,7 +2,6 @@ from langchain.tools import tool
 from bs4 import BeautifulSoup
 from tavily import TavilyClient
 from dotenv import load_dotenv
-from rich import print
 import requests
 import os
 
@@ -25,6 +24,7 @@ def web_search(query: str) -> str:
 
     return "\n----\n".join(out)
 
+
 @tool
 def scrape_url(url: str) -> str:
     """Scrape and return clean text content from a given URL for deeper reading."""
@@ -36,5 +36,3 @@ def scrape_url(url: str) -> str:
         return soup.get_text(separator=" ", strip=True)[:3000]
     except Exception as e:
         return f"Could not scrape URL: {str(e)}"
-
-print(scrape_url.invoke("enter-website-url"))
